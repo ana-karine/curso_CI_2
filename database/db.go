f1a974ba8f39e48413d4c83b9b2c966fe7c5043f
Ana Karine <anakarine.ti@gmail.com>
2024-06-03 15:28:20 -0300

    container e ambiente

--- database/db.go
+++ database/db.go
@@ -1,24 +1,34 @@
 package database
 
 import (
-	"log"
+    "log"
+    "os"
 
-	"github.com/guilhermeonrails/api-go-gin/models"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+    "github.com/guilhermeonrails/api-go-gin/models"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
 )
 
 var (
-	DB  *gorm.DB
-	err error
+    DB  *gorm.DB
+    err error
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
-	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
-	}
+    stringDeConexao := "host=" + os.Getenv("HOST") +
+        " user=" + os.Getenv("USER") +
+        " password=" + os.Getenv("PASSWORD") +
+        " dbname=" + os.Getenv("DBNAME") +
+        " port=" + os.Getenv("PORT") +
+        " sslmode=disable"
 
-	DB.AutoMigrate(&models.Aluno{})
+    DB, err = gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
+    if err != nil {
+        log.Panic("Erro ao conectar com banco de dados:", err)
+    }
+
+    err = DB.AutoMigrate(&models.Aluno{})
+    if err != nil {
+        log.Panic("Erro ao fazer auto-migrate:", err)
+    }
 }
